65df1bd581a6d419336c015cd3e12766fb4f64df
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-16 04:44:58 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,15 +10,11 @@ def find_item_by_name_in_collection(name, collection)
   return search_item
 end
 
-
-
 def consolidate_cart(cart)
   bagged_items=[]
-  #binding.pry
   cart.each do |item|
     #binding.pry
     lookup = find_item_by_name_in_collection(item[:item] , bagged_items);
-   #binding.pry
     if lookup == nil 
       item[:count] = 1
       bagged_items << item
@@ -27,13 +23,3 @@ def consolidate_cart(cart)
   end
   bagged_items
 end
-
-#cart is the shopping cart 
-
-#bagged items is the final product
-#look through each item in the cart
-#lookup to see if it is already in the bagged items
-#  if not, its added to the bagged items with a value of 1 
-#    if it is look through the bagged items for the item you have scanned
-#      if the item you scanned is equal to an item in the bag 
-#        bagged item count increases by 1
